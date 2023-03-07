a1f4d49b9cc991960b0319d945f59aa14f1694d6
Prerna Kale <121278313+prernakale06@users.noreply.github.com>
2023-03-07 23:09:23 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,20 +1,9 @@
-from django.urls import path,include
-from .import views
 
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-   path("", views.home, name="home"),
-   path("about.html", views.about, name="about"),
-   path("add_stock.html", views.add_stock, name="add_stock"),
-   path("delete/<stock_id>", views.delete, name="delete"),
-   path("delete_stock.html", views.delete_stock, name="delete_stock"),
-   path("registration/login/.html", views.login, name="login"),
-   path("ustock.html", views.ustock, name="ustock"),
-   # path("dashboard.html",views.dashboard, name="dashboard"),
-   path("main.html",views.main,name="main"),
-   path("contact.html",views.contact,name="contact"),
-   path("calculator.html",views.calculator,name="calculator"),
-   path("accounts/",include('accounts.urls')),
-   
+    path('admin/', admin.site.urls),
+    path('',include('quotes.urls')),
+    path("accounts/",include("django.contrib.auth.urls")),
 ]
-
